576bd98d806ba08996cffedea3aad79e22f2c828
yurialeatorio732-web <yurialeatorio732@gmail.com>
2026-07-31 13:56:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/v1/chat/completions', async (req, res) => {
     const nimRequest = {
       model: nimModel,
       messages: messages,
-      temperature: temperature || 0.65,
+      temperature: temperature || 0.6,
       max_tokens: max_tokens || 9024,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: stream || false
