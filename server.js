d258daa8f282677b56d1a18d5302758a262f1d6f
yurialeatorio732-web <yurialeatorio732@gmail.com>
2026-08-02 23:22:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
-  'gpt-4-turbo': 'minimaxai/minimax-m3',
+  'gpt-4-turbo': 'meta/llama-3.2-1b-instruct',
   'gpt-4o': 'deepseek-ai/deepseek-v4-flash',
   'claude-3-opus': 'openai/gpt-oss-120b',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
