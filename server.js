18bd5f35ce66ab72e808ac97d53174a72194d3b2
yurialeatorio732-web <yurialeatorio732@gmail.com>
2026-08-08 01:31:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const MODEL_MAPPING = {
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
   'gpt-4-turbo': 'nvidia/nemotron-3-super-120b-a12b',
   'gpt-4o': 'deepseek-ai/deepseek-v4-flash',
-  'claude-3-opus': 'DavidAU/Llama3.3-8B-Instruct-Thinking-Heretic-Uncensored-Claude-4.5-Opus-High-Reasoning',
+  'claude-3-opus': 'z-ai/glm-5.2',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking'
 };
